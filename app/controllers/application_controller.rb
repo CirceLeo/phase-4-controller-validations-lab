65289781bc0615435144bd3c5fe6b1413bbd9e70
Calypso Leonard <calypsorleonard@gmail.com>
2022-04-26 14:19:19 -0400

    commit tree a2d85ef707f094fa48df05aad5f47195e352e77b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordInvalid, with: :bad_data
+
+    private
+    def bad_data invalid
+        render json: {errors: invalid.record.errors.full_messages}, status: unprocessable_entity
+    end
 end
